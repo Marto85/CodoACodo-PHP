e384cd9669d7cd946c97d144c3803e35d1b08d86
MartoJuanDev <martojuandev@gmail.com>
2023-12-20 17:33:19 -0300

    cambios en la vista de edit_event

--- views/edit_event.php
+++ views/edit_event.php
@@ -45,6 +45,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr@4.6.6/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="https://kit.fontawesome.com/c4ac9449c7.js" crossorigin="anonymous"></script>
     <title>Eventia</title>
@@ -54,59 +55,74 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     <?php
     include '../views/partials/navbar.php';
     ?>
-    <div class="container">
-        <div class="section-title">
-            <h3 class="text-center">Editar Evento</h3>
-        </div>
-        <div class="event-form">
-            <form action="../controllers/edit_event.php" method="POST" enctype="multipart/form-data">
-                <input type="hidden" name="evento_id" value="<?php echo $eventoId; ?>">
-                <div class="mb-3">
-                    <label for="titulo" class="form-label">Título</label>
-                    <input type="text" name="titulo" class="form-control" value="<?php echo $titulo; ?>" required>
+    <div class="form-container container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="section-title">
+                    <h3 class="text-center">Editar Evento</h3>
                 </div>
-                <div class="mb-3">
-                    <label for="fecha" class="form-label">Fecha</label>
-                    <input type="date" name="fecha" class="form-control" value="<?php echo $fecha; ?>" required>
-                    <label for="lugar" class="form-label">Lugar</label>
-                    <input type="text" name="lugar" class="form-control" value="<?php echo $lugar; ?>" required>
-                    <div class="mb-3">
-                        <label for="path_imagen" class="form-label">Imagen Actual</label>
-                        <img src="<?php echo $imagen; ?>" alt="Imagen actual" style="max-width: 100%; height: auto;">
-                    </div>
-                    <div class="mb-3">
-                        <label for="nueva_imagen" class="form-label">Nueva Imagen (opcional)</label>
-                        <input type="file" name="nueva_imagen" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <select name="categoria" class="form-select" required>
-                            <option value="" disabled selected>Categoría</option>
-                            <option value="musica" <?php echo ($categoria === 'musica') ? 'selected' : ''; ?>>Música</option>
-                            <option value="teatro" <?php echo ($categoria === 'teatro') ? 'selected' : ''; ?>>Teatro</option>
-                            <option value="deportes" <?php echo ($categoria === 'deportes') ? 'selected' : ''; ?>>Deportes</option>
-                            <option value="conferencias" <?php echo ($categoria === 'conferencias') ? 'selected' : ''; ?>>Conferencias</option>
-                            <option value="otros" <?php echo ($categoria === 'otros') ? 'selected' : ''; ?>>Otros</option>
-                        </select>
+                <div class="form">
+                    <form action="../controllers/edit_event.php" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="evento_id" value="<?php echo $eventoId; ?>">
                         <div class="mb-3">
-                            <select name="importancia" class="form-select" required>
-                                <option value="" disabled selected>Importancia</option>
-                                <option value="ninguno" <?php echo ($importancia === 'ninguno') ? 'selected' : ''; ?>>Ninguno</option>
-                                <option value="destacado" <?php echo ($importancia === 'destacado') ? 'selected' : ''; ?>>Destacado</option>
-                                <option value="imperdible" <?php echo ($importancia === 'imperdible') ? 'selected' : ''; ?>>Imperdible</option>
-                            </select>
+                            <label for="titulo" class="form-label">Título</label>
+                            <input type="text" name="titulo" class="form-control" value="<?php echo $titulo; ?>" required>
                         </div>
                         <div class="mb-3">
-                            <button type="submit" class="btn btn-primary">Guardar Cambios</button>
-                            <a href="admin_panel.php" class="btn btn-secondary">Cancelar</a>
+                            <label for="fecha" class="form-label">Fecha</label>
+                            <input type="date" name="fecha" id="fecha" class="form-control" value="<?php echo $fecha; ?>" required>
+                            <label for="lugar" class="form-label">Lugar</label>
+                            <input type="text" name="lugar" class="form-control" value="<?php echo $lugar; ?>" required>
+                            <div class="mb-3">
+                                <!-- <label for="path_imagen" class="form-label">Imagen Actual</label> -->
+                                <img src="<?php echo $imagen; ?>" alt="Imagen actual" style="max-width: 100%; height: auto;">
+                            </div>
+                            <div class="mb-3">
+                                <label for="nueva_imagen" class="form-label">Nueva Imagen (opcional)</label>
+                                <input type="file" name="nueva_imagen" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <select name="categoria" class="form-select form-control" required>
+                                    <option value="" disabled selected>Categoría</option>
+                                    <option value="musica" <?php echo ($categoria === 'musica') ? 'selected' : ''; ?>>Música</option>
+                                    <option value="teatro" <?php echo ($categoria === 'teatro') ? 'selected' : ''; ?>>Teatro</option>
+                                    <option value="deportes" <?php echo ($categoria === 'deportes') ? 'selected' : ''; ?>>Deportes</option>
+                                    <option value="conferencias" <?php echo ($categoria === 'conferencias') ? 'selected' : ''; ?>>Conferencias</option>
+                                    <option value="otros" <?php echo ($categoria === 'otros') ? 'selected' : ''; ?>>Otros</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <select name="importancia" class="form-select form-control" required>
+                                    <option value="" disabled selected>Importancia</option>
+                                    <option value="ninguno" <?php echo ($importancia === 'ninguno') ? 'selected' : ''; ?>>Ninguno</option>
+                                    <option value="destacado" <?php echo ($importancia === 'destacado') ? 'selected' : ''; ?>>Destacado</option>
+                                    <option value="imperdible" <?php echo ($importancia === 'imperdible') ? 'selected' : ''; ?>>Imperdible</option>
+                                </select>
+                            </div>
+                            <div class="mb-3 text-center">
+                                <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+                                <button type="button" class="btn btn-primary custom">
+                                    <a href="admin_panel.php" style="text-decoration: none; color: inherit;">Cancelar</a>
+                                </button>
+                            </div>
                         </div>
-            </form>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-
-
+    <?php
+    include '../views/partials/footer.php';
+    ?>
+    <script src="../public/js/users.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr@4.6.6/dist/flatpickr.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            date();
+        });
+    </script>
+    <script src="../public/js/events.js"></script>
 </body>
-<?php
-include '../views/partials/footer.php';
-?>
+
 
 </html>
